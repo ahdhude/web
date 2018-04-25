4c6340974207b7c59ecf6b6505a52b5bafb228fc
ahdhude <ahdhude@gmail.com>
2018-04-25 17:45:07 +0500

    Add files via upload
    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="styles/index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+<link href="https://fonts.googleapis.com/css?family=Comfortaa|Josefin+Sans|Lato|Source+Sans+Pro|Tajawal|Titillium+Web|Ubuntu" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <script type="text/javascript" src="js/home.js"></script>
+
+</head>
+
+<body>
+  <?php
+  include('nav.html');
+  include ('carosel.html');
+  include('content.html');
+
+?>
+
+</body>
+
+</html>
---
+++ js/customer.js
@@ -0,0 +1,49 @@
+$( window ).on( "load", function() {
+//hides when form loads
+$('.upload-form').hide();
+
+
+   // SELECTS THE NAV BUTTON WHEN CLICKED
+
+   $('.icon-bar').on('click', 'a', function () {
+
+          if (this.text=='Upload') {
+            //change class so know which one is selected
+              $(".active").attr('class', '');
+              $(this).addClass("active");
+              $('#requests').hide();
+              $('.book-form').hide();
+              $('.upload-form').css("visibility","");
+              $('.upload-form').show();
+
+
+
+          }else {
+            //change class so know which one is selected
+            $(".active").attr('class', '');
+            $(this).addClass("active");
+            $('#requests').show();
+            $('.book-form').show();
+              $('.upload-form').hide();
+            $('.upload-form').css("visibility","hidden");
+
+
+          }
+      });
+
+
+
+
+
+   ///call light house
+
+
+
+});
+
+
+
+    $(document).ready(function() {
+        $("#lightgallery").lightGallery(); 
+    });
+
