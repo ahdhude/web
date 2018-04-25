f2b45bd184671733374b2501687d07958f564f1d
ahdhude <ahdhude@gmail.com>
2018-04-26 03:26:10 +0500

    fixed a bug

--- js/home.js
+++ js/home.js
@@ -46,6 +46,19 @@ $( document ).ready(function() {
 
 
 	//function to stick navbar when scrolled
+	//function to stick navbar when scrolled
+		window.onscroll = function() {myFunction()};
+
+		var navbar = document.getElementById("navbar");
+		var sticky = navbar.offsetTop;
+
+		function myFunction() {
+		  if (window.pageYOffset >= sticky) {
+		    navbar.classList.add("sticky")
+		  } else {
+		    navbar.classList.remove("sticky");
+		  }
+		}
 
 
 
